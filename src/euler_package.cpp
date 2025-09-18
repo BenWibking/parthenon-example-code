2590fc89799777d1264fe276d897005a0c2ba5b0
Ben Wibking <ben@wibking.com>
2025-09-18 18:07:49 -0400

    template HLL solver

--- src/euler_package.cpp
+++ src/euler_package.cpp
@@ -46,16 +46,85 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   return pkg;
 }
 
-static KOKKOS_INLINE_FUNCTION void cons_to_prim_x(
-    const Real gamma, const Real rho, const Real mx, const Real my, const Real mz,
+static KOKKOS_INLINE_FUNCTION void cons_to_prim(
+    const Real gamma, const Real rho, const Real mN, const Real mT1, const Real mT2,
     const Real E, Real *u, Real *v, Real *w, Real *p, Real *a) {
   const Real inv_rho = 1.0 / rho;
-  *u = mx * inv_rho;
-  *v = my * inv_rho;
-  *w = mz * inv_rho;
+  *u = mN * inv_rho;
+  *v = mT1 * inv_rho;
+  *w = mT2 * inv_rho;
   const Real ke = 0.5 * rho * ((*u) * (*u) + (*v) * (*v) + (*w) * (*w));
   *p = (gamma - 1.0) * (E - ke);
   *a = std::sqrt(std::max(Real(0.0), gamma * (*p) * inv_rho));
+}
+
+template <int DIR>
+KOKKOS_INLINE_FUNCTION void hll_flux_dir(
+    const Real gamma,
+    const Real rhoL, const Real mxL, const Real myL, const Real mzL, const Real EL,
+    const Real rhoR, const Real mxR, const Real myR, const Real mzR, const Real ER,
+    Real *F_rho, Real *F_mx, Real *F_my, Real *F_mz, Real *F_E) {
+  // Map momenta so mN is normal, mT1/mT2 tangential to the face normal
+  Real mN_L, mT1_L, mT2_L;
+  Real mN_R, mT1_R, mT2_R;
+  if constexpr (DIR == X1DIR) {
+    mN_L = mxL; mT1_L = myL; mT2_L = mzL;
+    mN_R = mxR; mT1_R = myR; mT2_R = mzR;
+  } else if constexpr (DIR == X2DIR) {
+    mN_L = myL; mT1_L = mxL; mT2_L = mzL;
+    mN_R = myR; mT1_R = mxR; mT2_R = mzR;
+  } else { // X3DIR (not used here, but keep generic)
+    mN_L = mzL; mT1_L = mxL; mT2_L = myL;
+    mN_R = mzR; mT1_R = mxR; mT2_R = myR;
+  }
+
+  Real uL, vL, wL, pL, aL; // u = normal velocity
+  Real uR, vR, wR, pR, aR;
+  cons_to_prim(gamma, rhoL, mN_L, mT1_L, mT2_L, EL, &uL, &vL, &wL, &pL, &aL);
+  cons_to_prim(gamma, rhoR, mN_R, mT1_R, mT2_R, ER, &uR, &vR, &wR, &pR, &aR);
+
+  const Real smax = std::max(std::abs(uL) + aL, std::abs(uR) + aR);
+
+  // Physical fluxes (left/right) in the normal direction
+  const Real Fr_rho_L = rhoL * uL;
+  const Real Fr_mN_L = mN_L * uL + pL;
+  const Real Fr_mT1_L = mT1_L * uL;
+  const Real Fr_mT2_L = mT2_L * uL;
+  const Real Fr_E_L = (EL + pL) * uL;
+
+  const Real Fr_rho_R = rhoR * uR;
+  const Real Fr_mN_R = mN_R * uR + pR;
+  const Real Fr_mT1_R = mT1_R * uR;
+  const Real Fr_mT2_R = mT2_R * uR;
+  const Real Fr_E_R = (ER + pR) * uR;
+
+  // HLL (Rusanov) flux for each conservative component
+  const Real F_rho_dir = 0.5 * (Fr_rho_L + Fr_rho_R) - 0.5 * smax * (rhoR - rhoL);
+  const Real F_mN_dir  = 0.5 * (Fr_mN_L + Fr_mN_R)   - 0.5 * smax * (mN_R - mN_L);
+  const Real F_mT1_dir = 0.5 * (Fr_mT1_L + Fr_mT1_R) - 0.5 * smax * (mT1_R - mT1_L);
+  const Real F_mT2_dir = 0.5 * (Fr_mT2_L + Fr_mT2_R) - 0.5 * smax * (mT2_R - mT2_L);
+  const Real F_E_dir   = 0.5 * (Fr_E_L + Fr_E_R)     - 0.5 * smax * (ER - EL);
+
+  // Map back to x,y,z ordering
+  if constexpr (DIR == X1DIR) {
+    *F_rho = F_rho_dir;
+    *F_mx  = F_mN_dir;
+    *F_my  = F_mT1_dir;
+    *F_mz  = F_mT2_dir;
+    *F_E   = F_E_dir;
+  } else if constexpr (DIR == X2DIR) {
+    *F_rho = F_rho_dir;
+    *F_mx  = F_mT1_dir;
+    *F_my  = F_mN_dir;
+    *F_mz  = F_mT2_dir;
+    *F_E   = F_E_dir;
+  } else { // X3DIR
+    *F_rho = F_rho_dir;
+    *F_mx  = F_mT1_dir;
+    *F_my  = F_mT2_dir;
+    *F_mz  = F_mN_dir;
+    *F_E   = F_E_dir;
+  }
 }
 
 parthenon::TaskStatus ComputeFluxes(std::shared_ptr<MeshBlockData<Real>> &rc) {
@@ -102,33 +171,17 @@ parthenon::TaskStatus ComputeFluxes(std::shared_ptr<MeshBlockData<Real>> &rc) {
           const Real myR  = pack(0, mom(1), k, j, iR);
           const Real mzR  = pack(0, mom(2), k, j, iR);
           const Real ER   = pack(0, E(), k, j, iR);
+          Real F_rho, F_mx, F_my, F_mz, F_E;
+          hll_flux_dir<X1DIR>(gamma,
+                              rhoL, mxL, myL, mzL, EL,
+                              rhoR, mxR, myR, mzR, ER,
+                              &F_rho, &F_mx, &F_my, &F_mz, &F_E);
 
-          Real uL, vL, wL, pL, aL; // normal is x
-          Real uR, vR, wR, pR, aR;
-          cons_to_prim_x(gamma, rhoL, mxL, myL, mzL, EL, &uL, &vL, &wL, &pL, &aL);
-          cons_to_prim_x(gamma, rhoR, mxR, myR, mzR, ER, &uR, &vR, &wR, &pR, &aR);
-          const Real smax = std::max(std::abs(uL) + aL, std::abs(uR) + aR);
-
-          const Real Fr_rho_L = rhoL * uL;
-          const Real Fr_mx_L = mxL * uL + pL;
-          const Real Fr_my_L = myL * uL;
-          const Real Fr_E_L = (EL + pL) * uL;
-
-          const Real Fr_rho_R = rhoR * uR;
-          const Real Fr_mx_R = mxR * uR + pR;
-          const Real Fr_my_R = myR * uR;
-          const Real Fr_E_R = (ER + pR) * uR;
-
-          pack.flux(0, 1, rho(), k, j, i) = 0.5 * (Fr_rho_L + Fr_rho_R) - 0.5 * smax * (rhoR - rhoL);
-          pack.flux(0, 1, mom(0), k, j, i) = 0.5 * (Fr_mx_L + Fr_mx_R) - 0.5 * smax * (mxR - mxL);
-          pack.flux(0, 1, mom(1), k, j, i) = 0.5 * (Fr_my_L + Fr_my_R) - 0.5 * smax * (myR - myL);
-          {
-            const Real Fr_mz_L = mzL * uL;
-            const Real Fr_mz_R = mzR * uR;
-            pack.flux(0, 1, mom(2), k, j, i) =
-                0.5 * (Fr_mz_L + Fr_mz_R) - 0.5 * smax * (mzR - mzL);
-          }
-          pack.flux(0, 1, E(), k, j, i) = 0.5 * (Fr_E_L + Fr_E_R) - 0.5 * smax * (ER - EL);
+          pack.flux(0, 1, rho(),   k, j, i) = F_rho;
+          pack.flux(0, 1, mom(0),  k, j, i) = F_mx;
+          pack.flux(0, 1, mom(1),  k, j, i) = F_my;
+          pack.flux(0, 1, mom(2),  k, j, i) = F_mz;
+          pack.flux(0, 1, E(),     k, j, i) = F_E;
         }
       });
 
@@ -157,32 +210,17 @@ parthenon::TaskStatus ComputeFluxes(std::shared_ptr<MeshBlockData<Real>> &rc) {
             const Real mzR  = pack(0, mom(2), k, jR, i);
             const Real ER   = pack(0, E(), k, jR, i);
 
-            Real uL, vL, wL, pL, aL; // normal is y
-            Real uR, vR, wR, pR, aR;
-            cons_to_prim_x(gamma, rhoL, mxL, myL, mzL, EL, &uL, &vL, &wL, &pL, &aL);
-            cons_to_prim_x(gamma, rhoR, mxR, myR, mzR, ER, &uR, &vR, &wR, &pR, &aR);
-            const Real smax = std::max(std::abs(vL) + aL, std::abs(vR) + aR);
+            Real F_rho, F_mx, F_my, F_mz, F_E;
+            hll_flux_dir<X2DIR>(gamma,
+                                rhoL, mxL, myL, mzL, EL,
+                                rhoR, mxR, myR, mzR, ER,
+                                &F_rho, &F_mx, &F_my, &F_mz, &F_E);
 
-            const Real Fr_rho_L = rhoL * vL;
-            const Real Fr_mx_L = mxL * vL;
-            const Real Fr_my_L = myL * vL + pL;
-            const Real Fr_E_L = (EL + pL) * vL;
-
-            const Real Fr_rho_R = rhoR * vR;
-            const Real Fr_mx_R = mxR * vR;
-            const Real Fr_my_R = myR * vR + pR;
-            const Real Fr_E_R = (ER + pR) * vR;
-
-            pack.flux(0, 2, rho(), k, j, i) = 0.5 * (Fr_rho_L + Fr_rho_R) - 0.5 * smax * (rhoR - rhoL);
-            pack.flux(0, 2, mom(0), k, j, i) = 0.5 * (Fr_mx_L + Fr_mx_R) - 0.5 * smax * (mxR - mxL);
-            pack.flux(0, 2, mom(1), k, j, i) = 0.5 * (Fr_my_L + Fr_my_R) - 0.5 * smax * (myR - myL);
-            {
-              const Real Fr_mz_L = mzL * vL;
-              const Real Fr_mz_R = mzR * vR;
-              pack.flux(0, 2, mom(2), k, j, i) =
-                  0.5 * (Fr_mz_L + Fr_mz_R) - 0.5 * smax * (mzR - mzL);
-            }
-            pack.flux(0, 2, E(), k, j, i) = 0.5 * (Fr_E_L + Fr_E_R) - 0.5 * smax * (ER - EL);
+            pack.flux(0, 2, rho(),   k, j, i) = F_rho;
+            pack.flux(0, 2, mom(0),  k, j, i) = F_mx;
+            pack.flux(0, 2, mom(1),  k, j, i) = F_my;
+            pack.flux(0, 2, mom(2),  k, j, i) = F_mz;
+            pack.flux(0, 2, E(),     k, j, i) = F_E;
           }
         });
   }
@@ -219,7 +257,7 @@ Real EstimateTimestepBlock(MeshBlockData<Real> *rc) {
         const Real mz   = pack(0, mom(2), k, j, i);
         const Real E_v  = pack(0, E(), k, j, i);
         Real u, v, w, p, a;
-        cons_to_prim_x(gamma, rho_v, mx, my, mz, E_v, &u, &v, &w, &p, &a);
+        cons_to_prim(gamma, rho_v, mx, my, mz, E_v, &u, &v, &w, &p, &a);
         Real inv_dt = 0.0;
         inv_dt = std::max(inv_dt, (std::abs(u) + a) / coords.Dxc<X1DIR>(k, j, i));
         if (pmb->pmy_mesh->ndim >= 2)
